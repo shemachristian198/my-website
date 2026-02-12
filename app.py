e8355620bcabd14d5286092901537e767ebd158b
shemachristian198 <shemachristian350@gmail.com>
2026-02-12 19:36:39 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -173,6 +173,6 @@ def private_message(data):
             "message": message
         }, room=online_users[receiver]["sid"])
 
-
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
